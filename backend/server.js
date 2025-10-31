328389eb183d6f38696fb0ce07495064855272b7
Chetan Patidar <cpatidar568@gmail.com>
2025-10-31 23:57:57 +0530

    added cookie-parser

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const dotenv=require('dotenv');
+const cookieParser=require('cookie-parser');
 const cors=require('cors');
 const main=require('./config/db');
 const authRouter = require('./routes/authRouter');
